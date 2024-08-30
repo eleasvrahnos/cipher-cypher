1a534bb91afd818854a8fa06978a2e1544db1339
Eleas Vrahnos <evrahnos1966@gmail.com>
2024-08-29 21:10:11 -0700

    beforeFiles test

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -6,12 +6,14 @@ const nextConfig = {
     const isDev = process.env.NODE_ENV === 'development';
     const backendUrl = (isDev ? "http://localhost:5000" : process.env.SERVER);
 
-    return [
-      {
-        source: "/apiList/:path*",
-        destination: `${backendUrl}/apiList/:path*`, // Proxy to backend
-      },
-    ];
+    return {
+      beforeFiles:[
+        {
+          source: "/apiList/:path*",
+          destination: `https://ciphercypher-server-6ff695f2e615545b.vercel.app/apiList/:path*`, // Proxy to backend
+        }
+      ],
+  };
   },
 };
 
