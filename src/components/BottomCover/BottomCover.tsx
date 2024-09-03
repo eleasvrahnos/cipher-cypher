5957b7b5349116cd6d990530c2973e2a2b3e5166
Eleas Vrahnos <evrahnos1966@gmail.com>
2024-09-03 15:39:39 -0700

    disallowed on mobile test

--- src/components/BottomCover/BottomCover.tsx
+++ src/components/BottomCover/BottomCover.tsx
@@ -1,6 +1,4 @@
-// BottomCover.tsx - Bottom Cover component on website startup
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import IconList from "../IconList/IconList";
 
 // onPlay is called when Bottom Cover Play button is clicked
@@ -10,14 +8,43 @@ interface BottomCoverProps {
 }
 
 const BottomCover: React.FC<BottomCoverProps> = ({ isPlaying, onPlay }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Function to check the screen width and update state
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth < 768); // Assuming 768px is the breakpoint for mobile devices
+    };
+
+    // Initial check
+    checkScreenSize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", checkScreenSize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", checkScreenSize);
+    };
+  }, []);
+
   return (
     <div
       className={`fixed bottom-0 left-0 right-0 z-50 flex h-1/2 items-start justify-center bg-black p-4 text-white transition-transform duration-700 ${
         isPlaying && "translate-y-[100%]"
       }`}
     >
-      <div className="flex flex-col gap-5 h-12">
-        <button className="border border-white p-2 rounded-xl hover:bg-white hover:text-black transition" onClick={onPlay}>Play</button>
+      <div className="flex flex-col items-center gap-5 h-12">
+        {isMobile ? (
+          <p className="text-center">Please use a desktop device to play.</p>
+        ) : (
+          <button
+            className="border border-white p-2 rounded-xl w-20 hover:bg-white hover:text-black transition"
+            onClick={onPlay}
+          >
+            Play
+          </button>
+        )}
         <IconList />
       </div>
     </div>
